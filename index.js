d5fa1548b8ebd95e02de5c909e2ce646fda08274
Thomas Alvarez <thomasalvarezx@gmail.com>
2022-07-06 22:22:41 -0300

    Correcciones

--- index.js
+++ index.js
@@ -4,22 +4,22 @@
  alert("Bienvenidos a la preventa de entradas para el concierto de Eminem")
  alert ("A continuación, ingrese solo el número de la fecha a la cual desea asistir")
 
-  let fechaUno = "24";
-  let fechaDos = "25";
+  let fechaUno = 24;
+  let fechaDos = 25;
 
 
  for (let i = 0; i <= 10; i++){
      let fecha = prompt("Ingrese a que fecha desea ir (24 o 25 de Febrero de 2023)");
     
-     if (fecha === "24"){
+     if (fecha === fechaUno){
          alert(`Usted a elegido la fecha ${fechaUno} de Febrero de 2023, Bienvenid@!`);
          break;
      }
-     if (fecha === "25"){
+     if (fecha === fechaDos){
          alert (`Usted a elegido la fecha ${fechaDos} de Febrero de 2023, Bienvenid@`);
          break;
      }
-     else (fecha != "24" || fecha != "25"); {
+     else (fecha != fechaUno && fecha != fechaDos); {
          alert("Usted no ha ingresado una fecha correcta");
      }
    
