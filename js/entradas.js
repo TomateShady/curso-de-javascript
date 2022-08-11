cb8f3ca003c39b8e5066f7381d1fc65d554a2cab
Thomas Alvarez <thomasalvarezx@gmail.com>
2022-08-11 17:34:08 -0300

    Agregamos fetch, pero no está funcionando

--- js/entradas.js
+++ js/entradas.js
@@ -1,65 +1,34 @@
-//alert("Bienvenidos a la preventa de entradas para el Nuevo Festival de Cine. A continuación, ingrese solo el número de la fecha a la cual desea asistir");
+//  alert("Bienvenidos a la preventa de entradas para el Nuevo Festival de Cine");
 
-let usuario;
-let usuarioStorage = sessionStorage.getItem("usuario");
- if(usuarioStorage){
-   let usuario = usuarioStorage;
-   let mensaje = `Bienvenid@ al festival ${usuario}`;
-   alert(mensaje);
- }else{
-   usuario = prompt("Ingrese el nombre de usuario que desea");
-   sessionStorage.setItem("usuario", usuario);
-   let contraseña = prompt ("Ingrese una contraseña");
-   let edad = parseInt(prompt("Ingrese su edad (solo numero)"));
-   alert(`Bienvenid@ al festival ${usuario}`)
- }
+//  let usuario;
+//  let usuarioStorage = sessionStorage.getItem("usuario");
+//   if(usuarioStorage){
+//     let usuario = usuarioStorage;
+//     let mensaje = `Bienvenid@ ${usuario}`;
+//     alert(mensaje);
+//   }else{
+//     usuario = prompt("Ingrese el nombre de usuario que desea");
+//     sessionStorage.setItem("usuario", usuario);
+//     let contraseña = prompt ("Ingrese una contraseña");
+//     let edad = parseInt(prompt("Ingrese su edad (solo numero)"));
+//     alert(`Bienvenid@ ${usuario}`)
+//   }
  
- //let permiso = usuario.edad >= 18 ? true : false;
- //  permiso ? console.log(`Bienvenid@ ${usuario}`): console.log("No puedes ingresar"); ;
+  const lista = document.getElementById("talonario")
 
-// let fechaUno = 24;
-// let fechaDos = 25;
-// let array1 = [];
-// let array2 = [];
-// let cantidadEntradas = array2;
-//      for (let i = 0; i < 10; i++){
-//          let fecha = parseInt(prompt(`Ingrese a que fecha desea ir (24 o 25 de Febrero de 2023)`));
-//          array1.push(fecha);
-       
+   fetch("./entradas.json")
+   .then(response => response.json())
+   .then(productos => {
+       productos.forEach(producto => {
+           const li = document.createElement("li");
+           li.innerHTML = `
+           <h2>${producto.nombre}</h2>
+           <p>${producto.precio}</p>
+           `;
 
-//      if (fecha === fechaUno){
-//          alert(`Usted ha elegido la fecha ${fechaUno} de Febrero de 2023, Bienvenid@!`);
-//          cantidadEntradas = parseInt(prompt(`Ingrese la cantidad de entradas. Máximo 3 por persona.`));
-//          array2.push(cantidadEntradas);
-//          break;
-//      }
-//      else if (fecha === fechaDos){
-//          alert (`Usted ha elegido la fecha ${fechaDos} de Febrero de 2023, Bienvenid@`);
-//          cantidadEntradas = parseInt(prompt(`Seleccione la cantidad de entradas. Máximo 3 por persona.`));
-//          array2.push(cantidadEntradas);
-//          break;
-//      }
-//      else (fecha != fechaUno && fecha != fechaDos); {
-//          alert(`Ingrese una fecha correcta`);
-//      }  
-   
-//  } 
-
-// let precioEntradas = 10000;
-// let descuento = 400;
-// let cuotas = parseInt(prompt("Ingrese en la cantidad de cuotas que desea abonar (3, 6 o 12)"));
-
-//      const suma = (a, b) => a + b;
-//      const resta = (a, b) => a - b;
-//      const multiplicar = (a, b) => a * b;
-//      const dividir = (a, b) => a / b;
-//      const iva = x => x * 0.21;
-
-// let resultado = resta(suma( multiplicar(precioEntradas, cantidadEntradas), iva(precioEntradas)), descuento);
-// let resultadoCuotas = dividir(resta(suma( multiplicar(precioEntradas, cantidadEntradas), iva(precioEntradas)), descuento), cuotas);
+           lista.append(li);
+       });
+   });
 
 
-// alert(`Fecha: ${array1} de Febrero de 2023. Entradas: ${array2}. Total a pagar: $${resultado} en ${cuotas} cuotas de $${resultadoCuotas} Pesos Argentinos`);
-
-//  console.log(array1, array2);
  
--- js/participar.js
+++ js/participar.js
@@ -1,33 +1,10 @@
 
-let formulario = document.getElementById("formulario");
-formulario.addEventListener("submit", validarFormulario);
+ let formulario = document.getElementById("formulario");
+     formulario.addEventListener("submit", validarFormulario);
+ 
+ let button = document.getElementById("btn")
 
-function validarFormulario(e){
-    e.preventDefault();
-    console.log(e.target.children[0].value);
-    console.log(e.target.children[1].value);
-    console.log(e.target.children[2].value);
-    
-    if(e.target.children[2].value.includes("@") && e.target.children[2].value.includes(".")){
-        let mensaje = document.createElement("div");
-        mensaje.innerHTML = "Tus material ha sido enviado correctamente";
-        mensaje.className = "verde";
-        document.body.append(mensaje);
-        e.target.children[0].value = "";
-        e.target.children[1].value = "";
-        e.target.children[2].value = "";
-    }else{
-        let mensaje = document.createElement("div");
-        mensaje.innerHTML = "Datos ingresados incorrectos";
-        mensaje.className = "rojo";
-        document.body.append(mensaje);
-        e.target.children[2].value = "";
-    }  
-}
-
-let button = document.getElementById("btn")
-
-button.addEventListener("click", () =>{
+  button.addEventListener("click", () =>{
     Swal.fire({
         title: '¿Desea continuar?',
         showDenyButton: true,
@@ -40,28 +17,66 @@ button.addEventListener("click", () =>{
         } else if (result.isDenied) {
           Swal.fire('El formulario no fue enviado', '', 'info')
         }
-      })
-});
+      })   
+    });
 
-// let timerInterval
-// Swal.fire({
-//     title: 'Auto close alert!',
-//     html: 'I will close in <b></b> milliseconds.',
-//     timer: 2000,
-//     timerProgressBar: true,
-//     didOpen: () => {
-//       Swal.showLoading()
-//       const b = Swal.getHtmlContainer().querySelector('b')
-//       timerInterval = setInterval(() => {
-//         b.textContent = Swal.getTimerLeft()
-//       }, 100)
-//     },
-//     willClose: () => {
-//       clearInterval(timerInterval)
-//     }
-//   }).then((result) => {
-//     /* Read more about handling dismissals below */
-//     if (result.dismiss === Swal.DismissReason.timer) {
-//       console.log('I was closed by the timer')
-//     }
-//   })
+    
+    function validarFormulario(e){
+      e.preventDefault();
+     
+      if(formulario){
+        let mensaje = document.createElement("div");
+              mensaje.innerHTML = "Tus material ha sido enviado correctamente";
+              mensaje.className = "verde";
+              document.body.append(mensaje);      
+          }else{
+              let mensaje = document.createElement("div");
+              mensaje.innerHTML = "Datos ingresados incorrectos";
+              mensaje.className = "rojo";
+              document.body.append(mensaje);
+            }  
+      }
+      validarFormulario();
+
+
+      
+      //----------------------Probamos de otra manera con el nuevo formulario---------------------------
+      // let usuario;
+      // let usuarioStorage = sessionStorage.getItem("usuario");
+      //      if(usuarioStorage){
+      //        let usuario = usuarioStorage;
+      //        let mensaje = `Bienvenid@ al festival ${usuario}`;
+      //        alert(mensaje);
+      //  }  else{
+      //       usuario = prompt("Ingrese el nombre de usuario que desea");
+      //       sessionStorage.setItem("usuario", usuario);
+      //       let contraseña = prompt ("Ingrese una contraseña");
+      //       let edad = parseInt(prompt("Ingrese su edad (solo numero)"));
+      //       alert(`Bienvenid@ al festival ${usuario}`)
+      //  }
+      
+
+
+
+      //  let timerInterval
+      //  Swal.fire({
+        //      title: 'Auto close alert!',
+//      html: 'I will close in <b></b> milliseconds.',
+//      timer: 2000,
+//      timerProgressBar: true,
+//      didOpen: () => {
+//        Swal.showLoading()
+//        const b = Swal.getHtmlContainer().querySelector('b')
+//        timerInterval = setInterval(() => {
+  //          b.textContent = Swal.getTimerLeft()
+  //        }, 100)
+//      },
+//      willClose: () => {
+//        clearInterval(timerInterval)
+//      }
+//    }).then((result) => {
+//      /* Read more about handling dismissals below */
+//      if (result.dismiss === Swal.DismissReason.timer) {
+  //        console.log('I was closed by the timer')
+//      }
+//    })
